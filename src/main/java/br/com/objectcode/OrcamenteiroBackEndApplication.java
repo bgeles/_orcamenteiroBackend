fb442a01151e112745dffb83e82f67e66a2f21b9
Bruno Glauco Eles <bgeles@gmail.com>
2019-02-14 14:50:19 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/objectcode/OrcamenteiroBackEndApplication.java
@@ -0,0 +1,14 @@
+package br.com.objectcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamenteiroBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamenteiroBackEndApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/br/com/objectcode/domain/Usuario.java
@@ -0,0 +1,5 @@
+package br.com.objectcode.domain;
+
+public class Usuario {
+
+}
